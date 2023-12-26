0e5eb93c9d778e332fbe68d9ac086276d8f57cb4
heera9331 <heera9331@gmail.com>
2023-12-26 20:37:31 +0530

    auto commit at Tue, Dec 26, 2023  8:37:31 PM

---
+++ XYZ/main.js
@@ -0,0 +1,49 @@
+// let str1 = "heera";
+// let str2 = new String("heera");
+
+// console.log(str1 == str2);
+// console.log(str1 === str2);
+
+// // Type coercion is the process of converting one data type to another during a comparison.
+
+// // comparision with coercion
+// console.log(1 == '1')
+
+// // comparision without coercion
+// console.log(1 === '1')
+
+// console.log('data is defined by user -> user defined data')
+
+// if(true) {
+//     let x = 10;
+//     console.log(x);
+// }
+
+// // can't access variable x
+// console.log(x); // error
+
+// const myFnx = async () => {
+//   console.log("hello");
+// };
+// myFnx();
+// console.log("hi");
+
+// const myFunc = () => {
+//     console.log('my arrow function invoked');
+// }
+
+// myFunc();
+
+// obj = {
+//   fname: "heera",
+//   lname: "singh",
+//   age: 20,
+//   printInfo: () => {
+//     console.log(fname);
+//     console.log(lname);
+//     console.log(age);
+//   },
+// };
+
+
+ 
