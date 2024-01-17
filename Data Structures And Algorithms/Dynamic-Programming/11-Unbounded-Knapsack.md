f8ee9a6c2aeb7b017e3cf2f74b0b2c5a3d775131
heera9331 <heera9331@gmail.com>
2024-01-17 14:12:03 +0530

    unbounded knapsack

--- Data Structures And Algorithms/Dynamic-Programming/11-Unbounded-Knapsack.md
+++ Data Structures And Algorithms/Dynamic-Programming/11-Unbounded-Knapsack.md
@@ -1,12 +1,12 @@
 # Unbounded Knapsack
 
-Given a kanpsack weight w and a set of n itemms with contain 
+Given a kanpsack weight w and a set of n itemms with contain
 value val[i] and weight wt[t], we need to calculate maximum
 amount that could make up this quanlity excatly is different
-from classical knapsack problem, 
+from classical knapsack problem,
 
-- In this type of problem we allowed to use unlimited number 
-of intances of items
+- In this type of problem we allowed to use unlimited number
+  of intances of items
 
 **Testcases**
 
@@ -24,8 +24,23 @@ Output:
 
 ```java
 
+// 0/1 knapsack
+int fnRec(int val[], int wt[],int i, int w, int n) {
+    if(i>=n) {
+        return 0;
+    }
+
+    if(wt[i]>w) {
+        return fnRec(val, wt, i+1, w, n);
+    }
+
+    int take=fnRec(val, wt, i, w-wt[i], n);
+    int notTake = fnRec(val, wt, i+1, w, n);
+    return Math.max(take, notTake);
+}
+
 int knapsack(int val[], int wt[], int w) {
-    
+    return fnRec(val, wt, w, val.length, 0);
 }
 
 ```
--- XYZ/main.py
+++ XYZ/main.py
@@ -383,23 +383,31 @@
 
 # print(sub_set_sum(a, _sum))
 
-def subset_sum(a: list[int], target: int):
-    n = len(a)
-    dp = [[False]*(target+1) for _ in range(n+1)]
+# def subset_sum(a: list[int], target: int):
+#     n = len(a)
+#     dp = [[False]*(target+1) for _ in range(n+1)]
 
-    for i in range(n+1):
-        dp[i][0] = True
+#     for i in range(n+1):
+#         dp[i][0] = True
 
-    for i in range(1, n+1):
-        for j in range(1, target+1):
-            if a[i-1] <= j:
-                dp[i][j] = dp[i-1][j] or dp[i-1][j-a[i-1]]
-            else:
-                dp[i][j] = dp[i-1][j]
+#     for i in range(1, n+1):
+#         for j in range(1, target+1):
+#             if a[i-1] <= j:
+#                 dp[i][j] = dp[i-1][j] or dp[i-1][j-a[i-1]]
+#             else:
+#                 dp[i][j] = dp[i-1][j]
 
-    return dp[n][target]
+#     return dp[n][target]
 
-a = [3, 3, 4, 4, 12, 5, 2]
-_sum = 9
+# a = [3, 3, 4, 4, 12, 5, 2]
+# _sum = 9
 
-print(subset_sum(a, _sum))
+# print(subset_sum(a, _sum))
+
+
+# import random as rnd
+
+# print(rnd.randint(1,1))
+# s = set([1,2,3])
+# # print(s.remove(3)/)
+# print(rnd.choices(list(set([1,2,3]))))
