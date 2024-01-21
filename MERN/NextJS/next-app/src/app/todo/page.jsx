79eaa880c6596a5324ec86981ad5c6daa9cd6185
heera9331 <heera9331@gmail.com>
2024-01-21 14:44:49 +0530

    isssue 1.0.1

--- MERN/NextJS/next-app/src/app/todo/page.jsx
+++ MERN/NextJS/next-app/src/app/todo/page.jsx
@@ -2,7 +2,7 @@ import Todo from "@/lib/models";
 
 // FETCH DATA WITH AN API
 const getData = async () => {
-  const res = await fetch("http://localhost:3000/api/todo", {
+  const res = await fetch("/api/todo", {
     next: { revalidate: 3600 },
   });
   console.log(res);
