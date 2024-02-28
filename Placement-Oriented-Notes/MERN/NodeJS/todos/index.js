14d7b478273abfd2a5fdaa3a97d2ea6f94b4a3d5
heera9331 <heera9331@gmail.com>
2024-02-28 11:26:42 +0530

    nodejs todos

--- Placement-Oriented-Notes/MERN/NodeJS/todos/index.js
+++ Placement-Oriented-Notes/MERN/NodeJS/todos/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import ejs from "ejs";
 import path from "path";
+import bodyParser from "body-parser";
 import { connectDB } from "./utils/index.js";
 
 dotenv.config();
@@ -9,17 +10,22 @@ const app = express();
 
 const db = await connectDB();
 
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Parse application/json
+app.use(bodyParser.json());
+
 app.set("view engine", "ejs");
 app.set("views", path.join(path.resolve("./"), "views"));
 app.use(express.static("views"));
 
-const todos = [
-  { id: 1, title: "DSA", desc: "description here" },
-  { id: 2, title: "Java", desc: "learn java programming" },
-];
-
 app.get("/", async (req, res) => {
   try {
+    console.log(db.collections());
+
+    console.log("todos", todos);
+
     res.render("index", { todos });
   } catch (error) {
     res.status(500).send("Internal Server Error");
@@ -28,13 +34,28 @@ app.get("/", async (req, res) => {
 
 app.get("/home", async (req, res) => {
   try {
+    console.log(db.collection("todos").find({}));
+    let todos = [];
     res.render("index", { todos });
   } catch (error) {
     res.status(500).send("Internal Server Error");
   }
 });
 
-app.post("/home", (req, res) => {});
+app.post("/home", async (req, res) => {
+  console.log("Received POST request");
+  console.log("Body:", req.body); // Log the request body
+
+  let todo = {};
+  todo.title = req.body.title;
+  todo.desc = req.body.desc;
+
+  console.log(todo);
+
+  let ack = await db.collection("todos").insertMany([todo]);
+
+  res.json(ack);
+});
 
 app.get("/about", async (req, res) => {
   try {
